6b8089a2dce2e636ffb6da3deb0684d372a10eb7
Ryan Bales <ryankbales@gmail.com>
2014-11-13 07:45:15 -0800

    putting it together, cannot get methods to load

--- game.rb
+++ game.rb
@@ -3,12 +3,13 @@ require_relative 'game_methods'
 CARD_VALUES = {two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, ten: 10, jack: 10, queen: 10, king: 10, ace: 11}
 
 class Deck
-  include ShuffleDeck, Cuttable, Dealable
-
+  include ShuffleDeck
+  include Cuttable
+  attr_accessor :deck
   def initialize(deck=[])
     card_ranks = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
     card_suits = ["hearts", "diamonds", "clubs", "spades"]
-    card_ranks.product(card_suits)
+    @deck = card_ranks.product(card_suits)
   end
 
 end
@@ -34,10 +35,9 @@ end
 
 class Game
   attr_accessor :player
+
   def initialize
-    puts "Welcome, what is your name?"
-    name = gets.chomp.capitalize
-    @player = Player.new(name)
+
   end
 
   def play
@@ -53,16 +53,26 @@ end
 
 class Blackjack < Game
   include Valuable
-  attr_accessor :deck
-  def initialize(dealer=Dealer.new, deck=Deck.new)
+  include Dealable
+  attr_accessor :deck, :dealer, :player
+  puts "Welcome, what is your name?"
+  name = gets.chomp.capitalize
+  player = Player.new(name)
+  dealer = Dealer.new
+  deck = Deck.new
+  def initialize(dealer, deck, player)
+    @player = player
     @dealer = dealer
     @deck = deck
     puts "Hello #{self.player.name}!  Your dealer is #{self.dealer.name}"
   end
 
-  player_hand = self.player.hand
-  dealer_hand = self.dealer.hand
-  self.player.get_hand_value(player_hand)
-  self.dealer.get_hand_value(player_hand)
-
+  deal_two_cards_each(deck, player.hand, dealer.hand)
+  player_hand = player.hand
+  dealer_hand = dealer.hand
+  binding.pry
+  get_hand_value(player_hand)
+  get_hand_value(dealer_hand)
 end
+
+Blackjack.new.play
